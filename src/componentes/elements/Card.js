a56fb2bbdc86a96ec8c0e16b637abc67a3d8e2a8
ThamiresFigueiredo <thamiresfcdejesus@gmail.com>
2023-07-23 15:18:11 -0300

    Dinamic name and Dinamic projects

--- src/componentes/elements/Card.js
+++ src/componentes/elements/Card.js
@@ -1,12 +1,27 @@
 import styles from './Cards.module.css'
 import ButtonB from './ButtonB'
+import {useState} from 'react'
 
 function Card({ video, title, tech, description, repo, site }) {
+
+    const [info, setInfo] = useState(false)
+
+    function infoOn(){
+        setInfo(true)
+    }
+
+    function infoOff(){
+        setInfo(false)
+    }
+
     return (
         <div className={styles.card}>
-            <a href={site} target='_blank'>
-            <video src={video} autoPlay loop muted />
+            <a onMouseLeave={infoOff} onMouseEnter={infoOn} href={site} target='_blank'>
+            <video src={video} autoPlay loop muted aLT='ERROR'/>
             </a>
+            
+            {info === true &&(
+
             <section>
                 <h3>{title}</h3>
                 <p><strong>{tech}</strong></p>
@@ -14,6 +29,7 @@ function Card({ video, title, tech, description, repo, site }) {
                 <ButtonB text='Acesse o repositório' link={repo}/>
                 <ButtonB text='Acesse o site' link={site}/>
             </section>
+            )}
         </div>
     )
 }
--- src/componentes/section/Presentation.js
+++ src/componentes/section/Presentation.js
@@ -2,14 +2,44 @@ import styles from './Presentation.module.css'
 import ButtonA from '../elements/ButtonA'
 import ButtonB from '../elements/ButtonB'
 import profile from '../../Images/profile-photo.png'
+import {useEffect, useState} from 'react'
 
 function Presentation () {
+const [text, setText] = useState('');
+const toRotate =['Thamires Figueiredo','Desenvolvedora Full Stack', 'Technical Product Manager'];
+const [loop, setLoop] = useState(0);
+const [isDeleting, setIsDeleting] = useState(false);
+const period = 100;
+const [delta, setDelta] = useState(100);
+
+useEffect(()=>{
+    let ticker = setInterval(()=>{toType()}, delta)
+    return()=> {clearInterval(ticker)}
+},[text])
+
+const toType = () =>{
+    let i = loop % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting ? fullText.substring(0,text.length-1) : fullText.substring(0,text.length+1);
+
+    setText(updatedText);
+
+    if(!isDeleting && updatedText === fullText){
+        setIsDeleting(true);
+        setDelta(period);
+    } else if (isDeleting && updatedText === ''){
+        setIsDeleting(false);
+        setDelta(period);
+        setLoop (loop+1);
+    }
+}
+
     return(
         <div className={styles.presentation} id='Presentation'>
              
              <div>
              <h4><strong>Bem-vindo(a) ao meu portifólio</strong></h4>
-             <h1>Meu nome é Thamires Figueiredo,</h1> 
+             <h1>Eu sou {text},</h1> 
         
                 <p>Eu sou product manager, mas também programadora full stack que fez o desevolvimento e design deste site.<br/>
                 Atuo na área de produtos há mais de 2 anos com enfoque em fintechs, atuando na área de negócios há mais de 4 anos, inclusive com iniciativas próprias de empreendedorismo. <br/>
